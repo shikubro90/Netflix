3c82c4db9d03b86644ea8be7b8d08f174b99b75d
shikubro90 <shikubro90@gmail.com>
2022-08-15 01:44:10 +0600

    update

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,15 +1,27 @@
 import './List.scss'
 import  {ArrowBackIosOutlined, ArrowForwardIosOutlined}  from '@material-ui/icons'
 import ListItem from '../ListItem/ListItem'
+import { useRef } from 'react'
 
 const List = () => {
+  const listRef = useRef()
+
+  const handleClick = (direction)=>{
+    const distance = listRef.current.getBoundingClientRect().x-50
+    if(direction === "left"){
+      listRef.current.style.transform=`translateX(${230 + distance}px)`
+    }else{
+      listRef.current.style.transform=`translateX(${-230 + distance}px)`
+    }
+  }
+  
+
   return (
     <div className='list'>
         <span className='listTitle'>Continue Watch</span>
-        
         <div className="wrapper">
-            <ArrowBackIosOutlined className='sliderArrow left'/>
-            <div className="container">
+            <ArrowBackIosOutlined className='sliderArrow left' onClick={(()=>{handleClick("left")})}/>
+            <div className="container" ref={listRef}>
                 <ListItem/>
                 <ListItem/>
                 <ListItem/>
@@ -23,7 +35,7 @@ const List = () => {
                 <ListItem/>
                 <ListItem/>
             </div>
-            <ArrowForwardIosOutlined className='sliderArrow right'/>
+            <ArrowForwardIosOutlined className='sliderArrow right' onClick={(()=>{handleClick("right")})} />
         </div>
     </div>
   )
